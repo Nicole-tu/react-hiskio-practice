5b8d41026e46ce81b7f8ce1a914389f35b85f359
Nicole Tu 杜晨寧 <1801665@C0699.local>
2020-09-24 15:59:08 +0800

    todo list

--- todo-hook-react/src/App.js
+++ todo-hook-react/src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import TodoList from './TodoList';
+import './App.css';
 
 // functional component + useState = class component
 // 透過組件呼叫順序紀錄取得state
 
-const App = () => {
-  // const [state, setState] = useState(0);
-
-  const [count, setCount] = useState(0);
-  const addCount = () => setCount(count + 1);
-
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={addCount}>Add</button>
-    </div>
-  );
-}
+const App = () => <TodoList />;
 
 export default App;
--- todo-hook-react/src/TodoList.js
+++ todo-hook-react/src/TodoList.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import TodoInput from './TodoInput';
+
+// functional component + useState = class component
+// 透過組件呼叫順序紀錄取得state
 
 const TodoList = () => {
-  const items = [];
+  // const [state, setState] = useState({初始值});
+  const [items, setItems] = useState([]);
+  const addItem = text => {
+    setItems([...items, { id: Date.now(), text, done: false }]);
+  };
+  const removeItem = id => {
+    let newArr = items.map(item => item.id === id ? { ...item, done: true } : item);
+    setItems([...newArr]);
+    setTimeout(() => setItems(items.filter(item => item.id !== id)), 1000);
+  };
   return (
-    <div>
-      <TodoInput />
-      <ul>
-        {item.map(item => (
-          <TodoItem key={item.id}>{item.text}</TodoItem>
-        ))}
-      </ul>
+    <div className="todolist">
+      <div className="heading">
+        <h1 className="title">To-Do List</h1>
+      </div>
+      <TodoInput addItem={addItem} />
+      <div className="items">
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              <span><a className={item.done ? 'done' : 'check'} onClick={() => removeItem(item.id)} ><i className="far fa-check-circle" /></a></span>
+              <span className={item.done ? 'list-done' : null}>{item.text}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 };
 
 export default TodoList;
